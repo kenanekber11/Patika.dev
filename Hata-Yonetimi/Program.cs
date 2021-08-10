cd9f0eba37ad26da8e7ef47511e1b1907712ed98
kenanekber11 <79718078+kenanekber11@users.noreply.github.com>
2021-08-11 00:28:05 +0300

    Add files via upload

---
+++ Hata-Yonetimi/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Hata_Yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* 
+            try
+            {
+            Console.WriteLine("Bir sayı giriniz");
+            int sayi1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı " +sayi1); 
+            }
+             catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+             finally
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }
+
+
+             try
+            {
+                int a= int.Parse(null);
+
+            }
+             catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex.Message.ToString());
+            }
+             finally
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }
+
+
+             try
+            {
+                int a= int.Parse("test");
+
+            }
+             catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex.Message.ToString());
+            }
+                 catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex.Message.ToString());
+            }
+             finally
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }
+*/
+
+            
+             try
+            {
+                int a= int.Parse("-2000000000000");
+
+            }
+             catch(OverflowException ex)
+            {
+                Console.WriteLine("çok büyük ya da çok küçük değer girdiniz..");
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
