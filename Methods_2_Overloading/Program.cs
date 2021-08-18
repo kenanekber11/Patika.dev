4a3504cec92a6b71ec5f63a83dcfc3ef2c1ad03c
kenanekber11 <79718078+kenanekber11@users.noreply.github.com>
2021-08-18 13:45:03 +0300

    Add files via upload

---
+++ Methods_2_Overloading/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Methods_2_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi="999";
+
+            bool sonuc= int.TryParse(sayi, out int result);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+            Metotlar instance=new Metotlar();
+            instance.Topla(6,8,out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            int ifade=999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Kenan", "Akbarov");
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a,int b, out int toplam)
+        {
+            toplam=a+b;
+        }   
+
+        public void EkranaYazdir(string veri){
+            Console.WriteLine(veri);
+        } 
+         public void EkranaYazdir(int veri){
+            Console.WriteLine(veri);
+        } 
+         public void EkranaYazdir(string veri1,string veri2){
+            Console.WriteLine(veri1 + veri2);
+        } 
+
+    }
+}
