a9aa3e1e2e4320e399dca6f27f8aedbd91eaf64d
kenanekber11 <79718078+kenanekber11@users.noreply.github.com>
2021-08-18 23:25:54 +0300

    Add files via upload
    DateTime And Math Methods

---
+++ StringMathMethods/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace StringMathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+            Console.WriteLine(DateTime.Now.AddDays(2));
+            Console.WriteLine(DateTime.Now.AddMonths(5));
+            Console.WriteLine(DateTime.Now.AddYears(6));
+            Console.WriteLine(DateTime.Now.AddHours(10));
+            Console.WriteLine(DateTime.Now.AddMinutes(12));
+            Console.WriteLine(DateTime.Now.AddSeconds(21));
+
+            Console.WriteLine(DateTime.Now.ToString("dd"));
+            Console.WriteLine(DateTime.Now.ToString("ddd"));
+            Console.WriteLine(DateTime.Now.ToString("dddd"));
+
+            Console.WriteLine(DateTime.Now.ToString("MM"));
+            Console.WriteLine(DateTime.Now.ToString("MMM"));
+            Console.WriteLine(DateTime.Now.ToString("MMMM"));
+
+            Console.WriteLine(DateTime.Now.ToString("yy"));
+            Console.WriteLine(DateTime.Now.ToString("yyyy"));
+        
+
+            Console.WriteLine(Math.Abs(-25));
+            Console.WriteLine(Math.Sin(90));
+            Console.WriteLine(Math.Cos(180));
+            Console.WriteLine(Math.Tan(0));
+
+            Console.WriteLine(Math.Ceiling(5.4));
+            Console.WriteLine(Math.Floor(5.6));
+            Console.WriteLine(Math.Round(5.5));
+            
+            Console.WriteLine(Math.Max(8,10));
+            Console.WriteLine(Math.Min(8,10));
+            
+            Console.WriteLine(Math.Pow(4,3));
+            Console.WriteLine(Math.Sqrt(81));
+            Console.WriteLine(Math.Log(10));
+            Console.WriteLine(Math.Exp(3));
+            Console.WriteLine(Math.Log10(10));
+
+        }
+    }
+}
