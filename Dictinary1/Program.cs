ebd78cf9e4cdd2f97e06c45cae0fe7a1a9daaea1
kenanekber11 <79718078+kenanekber11@users.noreply.github.com>
2021-08-19 16:40:05 +0300

    Dictionary

---
+++ Dictinary1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictinary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<int,string> users=new Dictionary<int, string>();
+            users.Add(12,"Ayşe Yılmaz");
+            users.Add(15,"Ahmet Yılmaz");
+            users.Add(18,"Deniz Arda");
+            users.Add(20,"Özcan Coşar");
+
+            Console.WriteLine(users[12]);
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user);
+            }
+
+            // Count
+            Console.WriteLine(users.Count);
+
+            Console.WriteLine(users.ContainsKey(12));
+            Console.WriteLine(users.ContainsValue("Arda"));
+
+            // Remove 
+            Console.WriteLine("*****Remove*****");
+            users.Remove(12);
+            
+            foreach (var user in users)
+                Console.WriteLine(user.Key);        
+
+            // Keys 
+            foreach (var user in users.Keys)
+                Console.WriteLine(user);
+    
+
+            // Values
+            foreach (var user in users.Values)
+                Console.WriteLine(user);
+        
+        }
+    }
+}
